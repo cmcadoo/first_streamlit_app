ff195346dc364f0178b96a2c38136cb5d299c172
cmcadoo <49218790+cmcadoo@users.noreply.github.com>
2023-09-29 13:38:26 -0400

    Update streamlit_app.py
    add new text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
@@ -44,3 +45,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
